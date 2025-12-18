28539e24912ab028b7826ae3fd280a1ecd63a088
gpollast <gpollast@student.42.fr>
2025-12-18 18:12:52 +0100

    (feat): add print arg

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 11:23:25 by gpollast          #+#    #+#             */
-/*   Updated: 2025/12/18 14:31:46 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/12/18 17:10:18 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 
 int	main(int ac, char **av)
 {
-	(void)av;
 	if (ac == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
+	}
+	for (size_t i = 1; av[i]; i++)
+	{
+		std::cout << av[i] << std::endl;
 	}
 	return (0);
 }
